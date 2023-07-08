e5aa4b5eabc2bb1131a379483207c1b6003baa19
Nmy81244 <naomy.viest@gmail.com>
2023-07-07 20:49:27 -0500

    First Commit

---
+++ src/main/java/com/acenaomy/acetase/AceTASEClient.java
@@ -0,0 +1,10 @@
+package com.acenaomy.acetase;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AceTASEClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
